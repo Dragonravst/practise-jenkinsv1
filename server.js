f67b65fe6f467d5e35c6992c6ef7a9f9b89dd4f1
Akash <git@grelinhealth.com>
2025-12-05 20:10:19 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+
+app.get('/hello', (req, res) => {
+  res.json({ message: 'Hello, World!' });
+});
+app.get('/goodbye', (req, res) => {
+  res.json({ message: 'Goodbye, World!' });
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
