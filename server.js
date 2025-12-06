5a3e96298b2cdb2ffff4c765aca2ad372bb717d3
Dragonravst <109019352+Dragonravst@users.noreply.github.com>
2025-12-06 13:36:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,14 +2,17 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+app.get('/', (req, res) => {
+  res.json({ message: 'Welcome to the API' });
+});
 
 app.get('/hello', (req, res) => {
   res.json({ message: 'Hello, World!' });
 });
+
 app.get('/goodbye', (req, res) => {
   res.json({ message: 'Goodbye, World!' });
 });
-
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
